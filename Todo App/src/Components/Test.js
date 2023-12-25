f734337def5c776926fa40beebdac50ec45d4033
Siddharth Vekariya <siddharthvekariya510@gmail.com>
2023-12-25 17:01:56 +0530

    Task Completed

--- Todo App/src/Components/Test.js
+++ Todo App/src/Components/Test.js
@@ -5,11 +5,20 @@ import "./Style.css"
 const Test = () => {
     const input = useRef("")
 
+    // main array where all of the tasks will be stored
     const [item, setItem] = useState([])
+
+    // storing id in another state for update function
     const [editId, setEditId] = useState(null)
 
-
+    // storing values of completed items as "true" in check state
     const [check, setCheck] = useState(false)
+
+    // storing completed tasks in saperate array
+    const [completedItem, setCompletedItem] = useState([])
+
+    // storing active tasks in saperate array
+    const [activeItem, setActiveItem] = useState([])
 
 
 
@@ -18,6 +27,7 @@ const Test = () => {
         getItem()
     }, [])
 
+    // function for getting data from json file via json server
     const getItem = () => {
         axios.get(`http://localhost:8002/items`).then((response) => {
             setItem(response.data)
@@ -30,13 +40,13 @@ const Test = () => {
             if (input.current.value == "") {
                 alert("Please fill the data.")
             } else {
-                axios.post(`http://localhost:8002/items`, { taskname: input.current.value }).then(() => {
+                axios.post(`http://localhost:8002/items`, { taskname: input.current.value, completed: false }).then(() => {
                     getItem()
                     input.current.value = ""
                 })
             }
         } else {
-            axios.put(`http://localhost:8002/items/${editId}`, { taskname: input.current.value }).then(() => {
+            axios.put(`http://localhost:8002/items/${editId}`, { taskname: input.current.value, completed: false }).then(() => {
                 getItem()
                 input.current.value = ""
                 setEditId(null)
@@ -64,22 +74,61 @@ const Test = () => {
         document.getElementById("addTaskBtn").innerHTML = "Update Task"
     }
 
-
+    // setting value as "true" in button checked tasks
     const statusChange = (id) => {
         let editCheck = item.filter((res) => {
-            return res.id = id
+            return res.id == id
         })
-        console.log(editCheck);
-        setCheck(editCheck[id].completed = true)
-        console.log(check);
+        // console.log(editCheck);
+        setCheck(editCheck[0].completed = true)
+        // console.log(check);
+        console.log(item);
+        input.current.value = editCheck[0].taskname
 
+        axios.put(`http://localhost:8002/items/${id}`, { taskname: input.current.value, completed: check }).then(() => {
+            getItem()
+            input.current.value = ""
+        })
+    }
+
+    // filtered and stored completed tasks on json file
+    const completedTask = () => {
+        let filteredItem = item.filter((res) => {
+            return (
+                res.completed == true
+            )
+        })
+        // console.log(filteredItem);
+        // setCompletedItem(filteredItem);
+        // console.log(completedItem);
+
+        axios.post(`http://localhost:8002/completedItems`, { taskname: input.current.value, completed: true }).then(() => {
+            axios.get(`http://localhost:8002/completedItems`).then(() => {
+                setCompletedItem(filteredItem);
+            })
+        })
+    }
+
+    // filtered and stored active tasks 
+    const active = () => {
+        let activeItem = item.filter((res) => {
+            return (
+                res.completed == false
+            )
+        })
+
+        axios.post(`http://localhost:8002/activeItem`, { taskname: input.current.value, completed: false }).then(() => {
+            axios.get(`http://localhost:8002/activeItem`).then(() => {
+                setActiveItem(activeItem);
+            })
+        })
     }
     return (
         <>
             <div>
                 <div className='container-fluid'>
                     <div className="container">
-                        <div className="h1 text-center m-5">
+                        <div className="h1 text-center m-5 text-light">
                             <i class="fa-solid fa-list-check"></i>&nbsp; ToDo App</div>
                         <div className="mt-4 text-center">
                             <input ref={input} className='w-50' type="text" placeholder='Enter Task Name' />
@@ -95,6 +144,7 @@ const Test = () => {
                 </div>
 
                 <div className='w-100'>
+                    <h3 className='text-center'>All Tasks</h3>
                     {
                         item.map((data, index, value) => {
                             return (
@@ -106,7 +156,7 @@ const Test = () => {
                                             <td className='text-start px-3 text-warning' >{index + 1}</td>
 
                                             <td className='text-start px-3 text-warning fw-light w-75'>{data.taskname}</td>
-                                            <td className='w-25'>
+                                            <td className='px-3'>
                                                 <button onClick={() => {
                                                     statusChange(data.id)
                                                 }} className='btn'>
@@ -135,6 +185,94 @@ const Test = () => {
                             )
                         })
                     }
+                </div>
+
+
+                {/* Completed Task starts from here */}
+                <div className='container text-center mt-4'>
+                    <button
+                        onClick={() => {
+                            completedTask()
+                        }}
+                        className='btn text-center w-25 me-2'>Completed</button>
+                    <button
+                        onClick={() => {
+                            active()
+                        }}
+                        className='btn text-center w-25 me-2'>Active</button>
+                </div>
+                <div className="row">
+
+                    <div className='col-6 text-center mt-3'>
+                        <h3 className='text-center'>Completed Tasks</h3>
+
+                        {
+                            completedItem.map((data, index, value) => {
+                                return (
+                                    <>
+                                        <table className='table table-hover border border-1 text-warning w-50'>
+                                            <tr className='d-flex justify-content-center align-items-center p-1' >
+
+                                                {/* index starts from 0 so we added (+1) to use it as a serial Number*/}
+                                                <td className='text-start px-3 text-warning' >{index + 1}</td>
+
+                                                <td className='text-start px-3 text-warning fw-light w-75'>{data.taskname}</td>
+                                                <td className='text-end'>
+                                                    <button className="btn text-center me-2"
+                                                        onClick={() => {
+                                                            updateItem(data.id)
+                                                        }}>
+                                                        <i className="fa-regular fa-pen-to-square p-1" title='Edit Task'></i>
+                                                    </button>
+                                                </td>
+                                                <td className='text-end '>
+                                                    <button className='btn ms-2'
+                                                        onClick={() => {
+                                                            deleteItem(data.id)
+                                                        }}>
+                                                        <i className="fa-solid fa-trash p-1" title='Delete Task'></i>
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </>
+                                )
+                            })
+                        }
+                    </div>
+
+
+
+                    <div className='col-6 text-center mt-3'>
+                        <h3 className='text-center'>Active Tasks</h3>
+
+                        {
+                            activeItem.map((data, index, value) => {
+                                return (
+                                    <>
+                                        <table className='table table-hover border border-1 text-warning w-50'>
+                                            <tr className='d-flex justify-content-center align-items-center p-1' >
+
+                                                {/* index starts from 0 so we added (+1) to use it as a serial Number*/}
+                                                <td className='text-start px-3 text-warning' >{index + 1}</td>
+
+                                                <td className='text-start px-3 text-warning fw-light w-75'>{data.taskname}</td>
+                                                <td className='text-end '>
+                                                    <button className='btn ms-2'
+                                                        onClick={() => {
+                                                            deleteItem(data.id)
+                                                        }}>
+                                                        <i className="fa-solid fa-trash p-1" title='Delete Task'></i>
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </>
+                                )
+                            })
+                        }
+                    </div>
+
                 </div>
             </div>
         </>
